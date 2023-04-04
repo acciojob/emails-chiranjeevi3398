06d398351dc267fa75068c3c9955d9dea08472cd
chiranjeevi3398 <gchiranjeevi3398@gmail.com>
2023-04-04 17:23:10 +0530

    Email Assignment Done

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,49 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(getPassword().equals(oldPassword))
+        {
+            if(isValid(newPassword))
+            {
+                this.password=newPassword;
+            }
+        }
+    }
+
+    public boolean isValid(String newPassword)
+    {
+        //checking if length is greater than 8
+        if(newPassword.length()<8)
+        {
+            return false;
+        }
+
+        //Checking if lower and uppercase characters are present
+        int upperCase=0;
+        int lowerCase=0;
+        int digit=0;
+        int special=0;
+        for(int i=0;i<newPassword.length();i++)
+        {
+            if(Character.isUpperCase(newPassword.charAt(i)))
+            {
+                upperCase++;
+            }
+            else if(Character.isLowerCase(newPassword.charAt(i)))
+            {
+                lowerCase++;
+            }
+            else if(Character.isDigit(newPassword.charAt(i)))
+            {
+                digit++;
+            }
+            else {
+                special++;
+            }
+        }
+        if(upperCase==0 || lowerCase==0 || digit==0 || special==0) return false;
+
+        return true;
     }
 }
